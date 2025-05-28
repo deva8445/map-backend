064f93f6f12ccf27a8c6626685a0a00256443405
Devashish Kumar <devashish.kumar8445@gmail.com>
2025-05-28 13:02:31 +0530

    manual integration

--- index.js
+++ index.js
@@ -69,6 +69,31 @@ app.post('/api/roads/snap-to-roads', async (req, res) => {
   }
 });
 
+app.post('/api/manual/calculate-distance', (req, res) => {
+  const path = req.body.path;
+
+  if (!Array.isArray(path) || path.length < 2) {
+    return res.status(400).json({ error: 'At least two GPS points are required.' });
+  }
+
+  let totalDistance = 0;
+  for (let i = 0; i < path.length - 1; i++) {
+    const prev = path[i];
+    const curr = path[i + 1];
+    totalDistance += haversineDistance({
+      latitude: prev.lat,
+      longitude: prev.lng
+    }, {
+      latitude: curr.lat,
+      longitude: curr.lng
+    });
+  }
+
+  console.log(totalDistance, "--------------------manual");
+
+  res.json({ distance: totalDistance.toFixed(2), from: "Manual Haversine" });
+});
+
 function haversineDistance(p1, p2) {
   const toRad = deg => deg * Math.PI / 180;
   const R = 6371;
